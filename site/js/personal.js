83de7079e2f5c33ceb0e5b5405ebd55df65ce13e
unknown <jedido101@yahoo.com>
2020-04-23 16:19:39 -0700

    add note fetching

--- site/js/personal.js
+++ site/js/personal.js
@@ -3,41 +3,54 @@
 
     let id = x => document.getElementById(x);
     let qs = x => document.querySelector(x);
-    let notes = [{
-            name: 'Jed Chen',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            name: 'George Huang',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            name: 'Leo Tsai',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        }
-    ];
+    let notes = [];
+    let vnote;
     let page;
-    let vnote = undefined;
+    let prisoner;
+    let initials;
     let cur = 0;
 
     function init() {
-        new Vue({
-            el: '#description',
-            data: {
-                title: 'Joyce "Joyce" Li',
-                wanted: '"updog"',
-                time: '10 years',
-                info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel bibendum diam. Aliquam lobortis, nisi sit amet consequat tempus, tortor augue egestas ex, sit amet fermentum metus lectus vel velit. Proin varius orci efficitur dui maximus, et dictum neque pharetra. Nulla facilisi. Sed vel facilisis diam, ut auctor est. Donec ullamcorper luctus nibh, ultricies consequat ligula vestibulum in. Aliquam vel enim lorem. Duis mattis odio arcu, vel vehicula velit iaculis ut. In malesuada tempus porta. Donec tincidunt accumsan felis et ultricies. Curabitur pellentesque mi id purus aliquet tempus. Pellentesque ullamcorper, odio eu posuere interdum, odio urna cursus elit, in molestie.'
-            }
-        });
         id('messages-img').addEventListener('click', getNotes);
         id('close').addEventListener('click', close);
         id('right').addEventListener('click', () => slide(1));
         id('left').addEventListener('click', () => slide(-1));
+        populateInfo();
     }
 
+    async function populateInfo() {
+        let title = new Vue({
+            el: 'title',
+            data: {
+                name: ''
+            }
+        });
+        let description = new Vue({
+            el: '#description',
+            data: {
+                title: '',
+                crime: '"updog"',
+                time: '10 years',
+                info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel bibendum diam. Aliquam lobortis, nisi sit amet consequat tempus, tortor augue egestas ex, sit amet fermentum metus lectus vel velit. Proin varius orci efficitur dui maximus, et dictum neque pharetra. Nulla facilisi. Sed vel facilisis diam, ut auctor est. Donec ullamcorper luctus nibh, ultricies consequat ligula vestibulum in. Aliquam vel enim lorem. Duis mattis odio arcu, vel vehicula velit iaculis ut. In malesuada tempus porta. Donec tincidunt accumsan felis et ultricies. Curabitur pellentesque mi id purus aliquet tempus. Pellentesque ullamcorper, odio eu posuere interdum, odio urna cursus elit, in molestie.'
+            }
+        });
+        let params = window.location.search;
+        params.replace('?', '').split('&').forEach((s) => {
+            let kv = s.split('=');
+            if (kv[0] === 'prisoner') {
+                initials = kv[1];
+            }
+        });
+        let names = await fetch('resources/names.json');
+        let json = await names.json();
+        prisoner = json[initials];
+        title.name = prisoner;
+        description.title = prisoner;
+    }
+
+    /* Note stuff */
+
     function close() {
-        console.log('close');
         id('notes-view').classList.add('hidden');
     }
 
@@ -52,34 +65,53 @@
     }
 
     function jump(index) {
-        cur = index;
-        vnote.name = notes[cur].name;
-        vnote.message = notes[cur].message;
-        page.num = `${cur + 1} / ${notes.length}`;
+        if (cur >= notes.length) {
+            // Wait 
+            setTimeout(() => jump(index), 100);
+        } else {
+            cur = index;
+            vnote.paragraphs = notes[cur].paragraphs;
+            vnote.name = notes[cur].name;
+            page.num = `${cur + 1} / ${notes.length}`;
+        }
     }
 
-    function getNotes() {
+    async function getNotes() {
         if (verify()) {
             if (vnote === undefined) {
-                // let response = await fetch('notes/notes.json');
-                // let json = await response.json();
                 vnote = new Vue({
                     el: '#note',
                     data: {
                         name: '',
-                        message: ''
+                        paragraphs: ['reading message...']
                     }
                 });
                 page = new Vue({
                     el: '#pagenum',
                     data: {
-                        num: ''
+                        num: '? / ?'
                     }
                 });
+                notes = [];
+                let response = await fetch('resources/notes/notes.json');
+                let json = await response.json();
+                json[initials].forEach(s => fetchNote(s));
             }
             jump(cur);
             id('notes-view').classList.remove('hidden');
         }
+    }
+
+    async function fetchNote(name) {
+        let response = await fetch(`resources/notes/${initials}_${name}.txt`);
+        let text = await response.text();
+        let lines = [];
+        text.split('\r\n').forEach((s) => {
+            if (s !== '') {
+                lines.push(s);
+            }
+        });
+        notes.push({ 'name': name, paragraphs: lines });
     }
 
     async function verify() {
